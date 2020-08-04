e4d885379e97d786a702d3e0b0900d34af624898
kioria95 <kenkioria@gmail.com>
2020-08-04 12:18:29 +0300

    Added Challenge 4

--- static/js/script.js
+++ static/js/script.js
@@ -142,3 +142,74 @@ function display(humanImageChoice, botImageChoice, finalMessage)
 var all_buttons = document.getElementsByTagName('button');
 console.log(all_buttons);
 
+var copyAllButtons = [];
+for (let i = 0; i < all_buttons.length; i++)
+{
+   copyAllButtons.push(all_buttons[i].classList[1]);
+}
+console.log(copyAllButtons);
+
+function buttonColorChange(button)
+{
+    if (button.value === 'red')
+    {
+        buttonsRed();
+    }
+
+    else if(button.value === 'green')
+    {
+        buttonsGreen();
+    }
+
+    else if(button.value === 'reset')
+    {
+        buttonsReset();
+    }
+
+    else if(button.value === 'random')
+    {
+        randomColors();
+    }
+
+
+}
+
+function buttonsRed()
+{
+    for (let i =0; i < all_buttons.length; i++)
+    {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-danger');
+    }
+}
+
+
+function buttonsGreen()
+{
+    for (let i =0; i < all_buttons.length; i++)
+    {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-success');
+    }
+}
+
+function buttonsReset()
+{
+    for (let i =0; i < all_buttons.length; i++)
+    {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButtons[i]);
+    }
+}
+
+function randomColors()
+{
+   let choices = ['btn-primary', 'btn-warning', 'btn-success', 'btn-danger'];
+
+    for (let i =0; i < all_buttons.length; i++)
+    {
+        let selection = Math.floor(Math.random () * 4) ;
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(choices[selection]);
+    }
+}
